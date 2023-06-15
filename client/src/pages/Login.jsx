6b9cf2085104ca568f91aa92e4ad3d33b8372631
unknown <solankikiran43300@gmail.com>
2023-06-15 11:15:53 +0530

    context API(login & logout)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import { useContext } from "react";
+import { AuthContext } from '../context/authContext';
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -11,6 +13,8 @@ const Login = () => {
   const [err, setError] = useState(null);
 
   const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
