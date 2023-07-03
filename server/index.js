c87189337b846ade5395cf8d353d2250828a7a5c
unknown <solankikiran43300@gmail.com>
2023-07-03 09:11:43 +0530

    create and update post

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import multer from 'multer';
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -12,6 +13,22 @@ dotenv.config({path : './config.env'});
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
